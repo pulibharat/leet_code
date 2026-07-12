7e3d383635b467e76a215d5cd815b3c7ab56d181
puli_bharat_149 <pulibharat2007@gmail.com>
2026-07-12 20:55:58 +0530

    Time: 1 ms (100%), Space: 48.1 MB (23.09%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,21 +1,46 @@
-class Solution {
-    public int[] singleNumber(int[] nums) {
+// class Solution {
+//     public int[] singleNumber(int[] nums) {
 
-        int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
+//         int n = nums.length;
+//         HashMap<Integer, Integer> map = new HashMap<>();
         
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
+//         for (int num : nums) {
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+//         }
 
-        int arr[] = new int[2];
-        int index = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == 1)
-                arr[index++] = entry.getKey();
-        }
+//         int arr[] = new int[2];
+//         int index = 0;
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//             if (entry.getValue() == 1)
+//                 arr[index++] = entry.getKey();
+//         }
       
 
-        return arr;
+//         return arr;
+//     }
+// }
+
+
+// 1. Why do we find the rightmost set bit?
+
+// Expected answer:
+
+// It identifies a bit where the two unique numbers differ. Using that bit, we can separate them into different groups.
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i=0;i<nums.length;i++){
+            xor^=nums[i];
+        }
+        int n = xor&(xor-1);
+        int mask =xor^n;
+        int b1 =0;int b2=0;
+        for(int ele : nums){
+            if((ele&mask)==0) b1^=ele;
+            else b2^=ele; 
+        }    
+        int[] ans ={b1,b2};
+        return ans;
     }
 }
