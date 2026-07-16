c3d760759f0e6b5c068e3984b5e5b8fcd36216ba
puli_bharat_149 <pulibharat2007@gmail.com>
2026-07-16 18:44:44 +0530

    Time: 4 ms (38.43%), Space: 43.2 MB (73.45%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            // Opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+
+            // Closing brackets
+            else {
+
+                if (stack.isEmpty())
+                    return false;
+
+                char opening = stack.peek();
+
+                if (ch == ')' && opening == '(')
+                    stack.pop();
+                else if (ch == '}' && opening == '{')
+                    stack.pop();
+                else if (ch == ']' && opening == '[')
+                    stack.pop();
+                else
+                    return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
