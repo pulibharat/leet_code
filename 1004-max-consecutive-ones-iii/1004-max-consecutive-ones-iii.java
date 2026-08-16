17bd444202f713e1bb967360b3b0a83ed60dbbb4
puli_bharat_149 <pulibharat2007@gmail.com>
2026-08-16 13:24:36 +0530

    Time: 4 ms (43.75%), Space: 52 MB (74.59%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -2,26 +2,53 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int n = nums.length;
-        // finding the max sub array with the max zeores]
-       
-        int max = 0;
-        for(int i=0; i<n; i++){
-             int zcnt =0;
-            for(int j=i; j<n; j++){
-                if(nums[j] == 0){
-                  zcnt++;
-                }
+        int l =0;
+        int r =0;
+        int max =0;
+        int zcnt =0;
 
-                if(zcnt > k){
-                    break;
-                }
-
-                int len = j -i +1;
-                max = Math.max(max,len);
-            }
-           
+        while(r<n){
+        
+        if(nums[r] == 0){
+            zcnt++;
         }
+        
+        while( zcnt > k){
+            if( nums[l] == 0){
+                zcnt--;
+            }
+            l++;
+        }
+        
+        int len = r - l +1;
+        max = Math.max(len,max);
+        r++;
+        }
+   
     return max;
     }
 }
+
+
+//   int n = nums.length;
+//         // finding the max sub array with the max zeores]
+       
+//         int max = 0;
+//         for(int i=0; i<n; i++){
+//              int zcnt =0;
+//             for(int j=i; j<n; j++){
+//                 if(nums[j] == 0){
+//                   zcnt++;
+//                 }
+
+//                 if(zcnt > k){
+//                     break;
+//                 }
+
+//                 int len = j -i +1;
+//                 max = Math.max(max,len);
+//             }
+           
+//         }
+//     return max;
 
