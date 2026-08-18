881a1a2b7d00f7544caeb7f40b82b9857f507162
puli_bharat_149 <pulibharat2007@gmail.com>
2026-08-18 21:05:14 +0530

    Time: 1 ms (99.96%), Space: 50.8 MB (66.45%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,27 +1,74 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
 
-        int n = nums.length;
+      public int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+      }
+
+    public int atMost(int[] nums, int goal) {
+
+      int n = nums.length;
+
+      if(goal < 0) return 0;
+        
+        int left =0;
+        int right =0;
         int cnt = 0;
+        int sum = 0;
 
-        for(int i = 0; i < n; i++) {
+        while(right < n){
+          
+          sum += nums[right];
 
-            int sum = 0;
-
-            for(int j = i; j < n; j++) {
-
-                sum += nums[j];
-
-                if(sum == goal) {
-                    cnt++;
-                }
-
-                if(sum > goal) {
-                    break;
-                }
-            }
+          while(sum > goal){
+            sum -= nums[left];
+            left++;
+          }
+          
+          cnt += right - left + 1;
+          right++;
         }
+    return cnt; 
+      }
 
-        return cnt;
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int numSubarraysWithSum(int[] nums, int goal) {
+
+//         int n = nums.length;
+//         int cnt = 0;
+
+//         for(int i = 0; i < n; i++) {
+
+//             int sum = 0;
+
+//             for(int j = i; j < n; j++) {
+
+//                 sum += nums[j];
+
+//                 if(sum == goal) {
+//                     cnt++;
+//                 }
+
+//                 if(sum > goal) {
+//                     break;
+//                 }
+//             }
+//         }
+
+//         return cnt;
+//     }
+// }
